606ef6ec5ac239601afa19ce76d0b0cceaf5226d
Peter McLarnan <croikle@gmail.com>
2012-10-24 18:56:11 -0400

    concatenation function

--- cryptoExamples.java
+++ cryptoExamples.java
@@ -17,6 +17,14 @@ class CipherTest {
     }
   }
 
+  static byte[] concat(byte[] left, byte[] right) {
+    byte[] out = new byte[left.length + right.length];
+    System.arraycopy(left, 0, out, 0, left.length);
+    System.arraycopy(right, 0, out, left.length, right.length);
+
+    return out;
+  }
+
 
   static void encryptTest(byte[] in, byte[] out) {
   // use this, or Buffered / Padded cipher?
