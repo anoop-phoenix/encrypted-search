8b1097a27f2dc935111c2be197ee3d565895d86f
Peter McLarnan <croikle@gmail.com>
2012-10-24 18:59:14 -0400

    now with random IV

--- cryptoExamples.java
+++ cryptoExamples.java
@@ -1,19 +1,27 @@
 import org.bouncycastle.crypto.modes.CBCBlockCipher;
 import org.bouncycastle.crypto.engines.AESEngine;
+import org.bouncycastle.crypto.params.ParametersWithIV;
 import org.bouncycastle.crypto.params.KeyParameter;
 import java.util.Arrays;
+import java.security.SecureRandom;
+import org.bouncycastle.util.encoders.Hex;
 
 class CipherTest {
+  static byte[] key = "abcdefhijklmnopq".getBytes();
+
   public static void main(String[] args) {
     byte[] input = "This is a test.@".getBytes();
-    byte[] encrypted = new byte[16];
-    byte[] decrypted = new byte[16];
+    byte[] encrypted;
+    byte[] decrypted;
 
-    encryptTest(input, encrypted);
-    decryptTest(encrypted, decrypted);
+    encrypted = encryptTest(input);
+    decrypted = decryptTest(encrypted);
 
     if(Arrays.equals(input,decrypted)) {
       System.out.println("Success!");
+    }
+    else {
+      System.out.println(new String(decrypted));
     }
   }
 
@@ -26,23 +34,32 @@ class CipherTest {
   }
 
 
-  static void encryptTest(byte[] in, byte[] out) {
+  static byte[] encryptTest(byte[] in) {
   // use this, or Buffered / Padded cipher?
-    byte[] key = "abcdefhijklmnopq".getBytes();
     CBCBlockCipher cipher = new CBCBlockCipher(new AESEngine());
 
-    // IV is probably important. change to ParameterWithIV eventually
-    cipher.init(true, new KeyParameter(key));
-    cipher.processBlock(in, 0, out, 0);
+    // get a random IV
+    SecureRandom random = new SecureRandom();
+    byte[] IV = new byte[16];
+    random.nextBytes(IV);
+    byte[] ciphertext = new byte[16];
+
+    cipher.init(true, new ParametersWithIV(new KeyParameter(key), IV));
+    cipher.processBlock(in, 0, ciphertext, 0);
+
+    return concat(IV, ciphertext);
   }
 
 
-  static void decryptTest(byte[] in, byte[] out) {
-    byte[] key = "abcdefhijklmnopq".getBytes();
+  static byte[] decryptTest(byte[] in) {
     CBCBlockCipher cipher = new CBCBlockCipher(new AESEngine());
 
-    // IV is probably important. change to ParameterWithIV eventually
-    cipher.init(false, new KeyParameter(key));
-    cipher.processBlock(in, 0, out, 0);
+    byte[] IV = Arrays.copyOfRange(in, 0, 16);
+    byte[] out = new byte[16];
+
+    cipher.init(false, new ParametersWithIV(new KeyParameter(key), IV));
+    cipher.processBlock(in, 16, out, 0);
+
+    return out;
   }
 }
