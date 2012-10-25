a540f19782f6ccb776ebae363114870a82b21f59
Peter McLarnan <croikle@gmail.com>
2012-10-24 22:40:21 -0400

    generate key randomly

--- CipherTest.java
+++ CipherTest.java
@@ -1,15 +1,17 @@
 import java.util.Arrays;
+import java.security.SecureRandom;
 
 class CipherTest {
-  // horribly low keyspace if we use only printable characters
-  // genkey using SecureRandom for real usage
-  static byte[] key = "abcdefhijklmnopq".getBytes();
 
   public static void main(String[] args) {
     byte[] input = "This is a test. Under 32.".getBytes();
+    SecureRandom randy = new SecureRandom();
+    byte[] key = new byte[16];
+    randy.nextBytes(key);
 
     byte[] encrypted = TwoBlockEncrypt.encrypt(input, key);
     byte[] decrypted = TwoBlockEncrypt.decrypt(encrypted, key);
+    // do we even use decryption in our program?
 
     if(Arrays.equals(input,decrypted)) {
       System.out.println("Success!");
