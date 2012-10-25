86f31865912a5d666f263b394db5433af4ef10cf
Peter McLarnan <croikle@gmail.com>
2012-10-24 23:07:45 -0400

    basic docs for PRF

--- PRF.java
+++ PRF.java
@@ -1,6 +1,16 @@
 import org.bouncycastle.crypto.macs.HMac;
 import org.bouncycastle.crypto.digests.SHA1Digest;
 import org.bouncycastle.crypto.params.KeyParameter;
+/*
+ * PRF
+ *
+ * A simple wrapper on HMAC-SHA1 implementing a keyed pseudorandom function
+ *
+ *   PRF(input, key)
+ * Input and key are arbitrary-length byte arrays.
+ * Output is 20 bytes. (160 bits of SHA1)
+ *
+ */
 
 public class PRF {
   static byte[] PRF(byte[] input, byte[] key) {
