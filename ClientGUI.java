d7fcd13736362cfb6b6b79d5e3db163ffeb571ab
Robobenny <rogersbc@muohio.edu>
2012-10-25 22:45:12 -0400

    Finished Client and Server programs, can send files to the server, can search them, and can get them back from a search. also added config files.

--- ClientGUI.java
+++ ClientGUI.java
@@ -16,31 +16,75 @@ class ClientGUI extends JFrame implements ActionListener{
 
     static int SEARCH_FLAG = 0;
     static int FILE_FLAG = 1;
+    static int ERROR_FLAG = -1;
+    static String CONFIG_FILE = "client.config";
  
-    String host = "localhost";
-    int sock = 6789;
+    String host;
+    int sock;
+    String directory;
 
+    JFrame frame = new JFrame();
     JPanel pane = new JPanel();
+    JLabel hostLabel = new JLabel("Hostname:");
+    JLabel sockLabel = new JLabel("Port Number:");
+    JTextField hostText = new JTextField(20);
+    JTextField sockText = new JTextField(20);
     JTextField searchText = new JTextField(20);
     JButton searchButton = new JButton("Search");
     JTextField fileText = new JTextField(20);
     JButton fileButton = new JButton("Upload File");
+    JTextField directoryText = new JTextField(20);
+    JButton directoryButton = new JButton("Set Directory");
 
     // sets up a very basic GUI
     ClientGUI() {
 	super("Remote Storage Client");
-	setBounds(100,100,300,200);
+	setBounds(100,100,400,200);
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	pane.setLayout(new GridLayout(5, 2, 5, 10));
 	Container con = this.getContentPane();
 	con.add(pane);
+
+	// Read in the config file
+	try {
+	    FileInputStream fstream = new FileInputStream(CONFIG_FILE);
+	    DataInputStream in = new DataInputStream(fstream);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+	    String strLine;
+	    host = br.readLine().substring(7);
+	    sock = Integer.parseInt(br.readLine().substring(9));
+	    directory = br.readLine().substring(12);
+	    
+	} catch (Exception e) {
+	    System.err.println("Execption: " + e.getMessage());
+	    JOptionPane.showMessageDialog(null, e.getMessage(), "IO Error", JOptionPane.ERROR_MESSAGE);
+	    host = "localhost";
+	    sock = 6789;
+	    directory = System.getProperty("user.dir");
+	}
+
+	hostText.setText(host);
+	sockText.setText(sock + "");
+	directoryText.setText(directory);
+
+	hostText.addActionListener(this);
+	sockText.addActionListener(this);
 	searchText.addActionListener(this);
 	searchButton.addActionListener(this);
 	fileText.addActionListener(this);
 	fileButton.addActionListener(this);
+	directoryText.addActionListener(this);
+	directoryButton.addActionListener(this);
+	pane.add(hostLabel);
+	pane.add(sockLabel);
+	pane.add(hostText);
+	pane.add(sockText);
 	pane.add(searchText);
 	pane.add(searchButton);
 	pane.add(fileText);
 	pane.add(fileButton);
+	pane.add(directoryText);
+	pane.add(directoryButton);
 	setVisible(true);
     }
 
@@ -97,7 +141,7 @@ class ClientGUI extends JFrame implements ActionListener{
 
 	message.add(filename);
 
-	FileInputStream fstream = new FileInputStream(filename);
+	FileInputStream fstream = new FileInputStream(directory + filename);
 	DataInputStream in = new DataInputStream(fstream);
 	BufferedReader br = new BufferedReader(new InputStreamReader(in));
 	String strLine;
@@ -111,6 +155,7 @@ class ClientGUI extends JFrame implements ActionListener{
 
 	String sentence;
 	String modifiedSentence;
+	int size;
 	BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
 	Socket clientSocket = new Socket(host, sock);
 	DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
@@ -123,7 +168,63 @@ class ClientGUI extends JFrame implements ActionListener{
 
 	outToServer.writeBytes(sentence);
 	modifiedSentence = inFromServer.readLine();
-	System.out.println("FROM SERVER: " + modifiedSentence);
+
+	// if File message
+	if (FILE_FLAG == Integer.parseInt(modifiedSentence)) {
+	    modifiedSentence = inFromServer.readLine();
+	    // if Error message
+	    if (ERROR_FLAG == Integer.parseInt(modifiedSentence)) {
+		JOptionPane.showMessageDialog(null, "Error writing file", "Server Error", JOptionPane.ERROR_MESSAGE);
+	    } else {
+		JOptionPane.showMessageDialog(null, "Successful file write", "Server Message", JOptionPane.PLAIN_MESSAGE);
+	    }
+	}
+	// if Search message
+	if (SEARCH_FLAG == Integer.parseInt(modifiedSentence)) {
+	    modifiedSentence = inFromServer.readLine();
+	    // if Error message
+	    if (ERROR_FLAG == Integer.parseInt(modifiedSentence)) {
+		JOptionPane.showMessageDialog(null, "Error searching for word", "Server Error", JOptionPane.ERROR_MESSAGE);
+	    } else {
+		size = Integer.parseInt(inFromServer.readLine());
+		message.clear();
+		sentence = "";
+
+		for (int i = 0; i < size; i++) {
+		    message.add(inFromServer.readLine());
+		    sentence += "\n" + message.get(i);
+		}
+
+		// Choose to weather or not to download found files
+		Object[] options = {"Download", "Cancel"};
+		int n = JOptionPane.showOptionDialog(frame, size + " files with matches:" + sentence + "\nDownload files?", "Server Message", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+		if (n == JOptionPane.YES_OPTION) {
+		    outToServer.writeBytes(FILE_FLAG + "\n" + size + sentence + "\n");
+		    try {
+			int fSize;
+			// download files
+			for (int i = 0; i < message.size(); i++) {
+			    fSize = Integer.parseInt(inFromServer.readLine());
+			    if (fSize == ERROR_FLAG) {
+				throw new Exception("Server Error");
+			    } else {
+				FileWriter fstream = new FileWriter(directory + message.get(i));
+				BufferedWriter out = new BufferedWriter(fstream);
+				for (int j = 0; j < fSize; j++) {
+				    out.write(inFromServer.readLine() + "\n");			
+				}
+				out.close();
+			    }
+			}
+		    } catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Error download files", "Server Error", JOptionPane.ERROR_MESSAGE);
+		    }
+		} else {
+		    outToServer.writeBytes(ERROR_FLAG + "\n");
+		}
+	    }
+	}
+
 	clientSocket.close();
 
     }
--- Server.java
+++ Server.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 /** 
     Server for CSE 667 Project 2
@@ -12,11 +13,59 @@ class Server {
 
     static int SEARCH_FLAG = 0;
     static int FILE_FLAG = 1;
+    static int ERROR_FLAG = -1;
+    static String CONFIG_FILE = "server.config";
 
     // Search handles searches of "searchString" and outputs a string
-    public static String Search(String searchString) {
-	//TODO
-	return "Searched for " + searchString + "\n";
+    public static String Search(String searchString, String directory) {
+
+	searchString = searchString.substring(0, searchString.indexOf("\n"));
+
+	ArrayList<String> matchingFiles = new ArrayList<String>();
+	
+	String retString = "";
+	
+	try {
+	    // get all text files in directory
+	    File folder = new File(directory);
+	    File[] listOfFiles = folder.listFiles();
+	    
+	    for (int i = 0; i < listOfFiles.length; i++) {
+		if (listOfFiles[i].isFile() && listOfFiles[i].toString().endsWith(".txt")) {
+		    // Search File
+		    FileInputStream fstream = new FileInputStream(listOfFiles[i]);
+		    DataInputStream in = new DataInputStream(fstream);
+		    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		    String strLine;
+		    while ((strLine = br.readLine()) != null) {
+			if (strLine.equals(searchString)) {
+			    // store only file name, not path
+			    strLine = listOfFiles[i].toString();
+			    if (strLine.indexOf("\\") >= 0) {
+				matchingFiles.add(strLine.substring(strLine.lastIndexOf("\\") + 1));
+			    } else {
+				if (strLine.indexOf("/") >= 0) {
+				    matchingFiles.add(strLine.substring(strLine.lastIndexOf("/") + 1));
+				}
+			    }
+
+			    break;
+			}
+		    }
+		}
+	    }
+
+	    retString += SEARCH_FLAG + "\n" + SEARCH_FLAG + "\n" + matchingFiles.size() + "\n";
+	    for (int i = 0; i < matchingFiles.size(); i++) {
+		retString += matchingFiles.get(i) + "\n";
+	    }
+
+	    return retString;
+
+	} catch (Exception e) {
+	    System.err.println("Execption: " + e.getMessage());
+	    return SEARCH_FLAG + "\n" + ERROR_FLAG + "\n";
+	}
     }
 
     // WriteFile  handles the writing of files passed from a client to a directory
@@ -33,27 +82,41 @@ class Server {
 		fileString = fileString.substring(fileString.indexOf("\n")+1);
 	    }
 	    out.close();
+	    
+	    return FILE_FLAG + "\n" + FILE_FLAG + "\n";
 	} catch (Exception e) {
 	    System.err.println("Error :" + e.getMessage());
+	    return FILE_FLAG+ "\n" + ERROR_FLAG + "\n";
 	}
-
-	return "Wrote file " + fileString + "\n";
     }
     
     // main communicates with the client
     public static void main(String argv[]) throws Exception {
 	
 	String directory;
-	if (argv.length > 0) {
-	    directory = argv[0];
-	} else {
-	    directory = "test/";
+	int sock;
+
+	// Read in the config file
+	try {
+	    FileInputStream fstream = new FileInputStream(CONFIG_FILE);
+	    DataInputStream in = new DataInputStream(fstream);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+	    String strLine;
+	    sock = Integer.parseInt(br.readLine().substring(9));
+	    directory = br.readLine().substring(12);
+	    
+	} catch (Exception e) {
+	    System.err.println("Execption: " + e.getMessage());
+	    sock = 6789;
+	    directory = System.getProperty("user.dir");
 	}
+
 	String clientSentence;
 	String capitalizedSentence;
 	int flag;
 	int size;
-	ServerSocket welcomeSocket = new ServerSocket(6789);
+	ArrayList<String> files = new ArrayList<String>();
+	ServerSocket welcomeSocket = new ServerSocket(sock);
 	
 	while(true) {
 	    
@@ -73,10 +136,47 @@ class Server {
 		clientSentence += inFromClient.readLine() + "\n";
 	    }
 
+	    // If client wants to search
 	    if (flag == SEARCH_FLAG) {
-		outToClient.writeBytes(Search(clientSentence));
+		files.clear();
+
+		// search
+		outToClient.writeBytes(Search(clientSentence, directory));
+
+		// if client wants to download found files
+		if (FILE_FLAG == Integer.parseInt(inFromClient.readLine())) {
+		    size = Integer.parseInt(inFromClient.readLine());
+		    for (int i = 0; i < size; i++) {
+			files.add(inFromClient.readLine());
+		    }
+		    System.out.println(files);
+		    
+		    // send files
+		    try {
+			for (int i = 0; i < files.size(); i++) {
+			    ArrayList<String> fileText = new ArrayList<String>();
+			    FileInputStream fstream = new FileInputStream(directory + files.get(i));
+			    DataInputStream in = new DataInputStream(fstream);
+			    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			    String strLine;
+			    while ((strLine = br.readLine()) != null) {
+				fileText.add(strLine);
+			    }
+			    
+			    outToClient.writeBytes(fileText.size() + "\n");
+			    for (int j = 0; j < fileText.size(); j++) {
+				outToClient.writeBytes(fileText.get(j) + "\n");
+			    }
+			    
+			}
+		    } catch (Exception e) {
+			System.err.println("Execption: " + e.getMessage());
+			outToClient.writeBytes(ERROR_FLAG + "\n");
+		    }
+		}
 	    }
 
+	    // If client want to store a file
 	    if (flag == FILE_FLAG) {
 		outToClient.writeBytes(WriteFile(clientSentence, directory));
 	    }
